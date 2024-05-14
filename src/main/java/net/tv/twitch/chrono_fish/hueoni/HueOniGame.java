64d125ac5dd7b2b8b5f81171ac02b2467ec03f5b
Daka-fish <yamatohidaka24@gmail.com>
2024-05-14 14:43:40 +0900

    runnersListとkillersListの追加

--- src/main/java/net/tv/twitch/chrono_fish/hueoni/HueOniGame.java
+++ src/main/java/net/tv/twitch/chrono_fish/hueoni/HueOniGame.java
@@ -1,12 +1,17 @@
 package net.tv.twitch.chrono_fish.hueoni;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HueOniGame {
-    private enum GameState{
+    public enum GameState{
         Running,
         Finished
     }
 
     GameState gameState;
+    List<HueOniPlayer> runnerList = new ArrayList<>();
+    List<HueOniPlayer> killerList = new ArrayList<>();
 
     HueOniGame(){
         this.gameState = GameState.Finished;
@@ -19,4 +24,10 @@ public class HueOniGame {
     public void setGameState(GameState gameState) {
         this.gameState = gameState;
     }
+
+    public void putRunList(HueOniPlayer player){runnerList.add(player);}
+    public void putKillList(HueOniPlayer player){killerList.add(player);}
+
+    public int getRunners(){return runnerList.size();}
+    public int getKillers(){return killerList.size();}
 }
