c8e55390cb6e79945b6cabc5196905b7f0b24962
Daka-fish <yamatohidaka24@gmail.com>
2024-05-17 13:25:19 +0900

    フィールド値の修正

--- src/main/java/net/tv/twitch/chrono_fish/hueoni/HueOniGame.java
+++ src/main/java/net/tv/twitch/chrono_fish/hueoni/HueOniGame.java
@@ -11,7 +11,7 @@ public class HueOniGame {
 
     GameState gameState;
     List<HueOniPlayer> runnerList = new ArrayList<>();
-    List<HueOniPlayer> killerList = new ArrayList<>();
+    List<HueOniPlayer> chaserList = new ArrayList<>();
 
     HueOniGame(){
         this.gameState = GameState.Finished;
@@ -20,14 +20,13 @@ public class HueOniGame {
     public GameState getGameState() {
         return gameState;
     }
-
     public void setGameState(GameState gameState) {
         this.gameState = gameState;
     }
 
     public void putRunList(HueOniPlayer player){runnerList.add(player);}
-    public void putKillList(HueOniPlayer player){killerList.add(player);}
+    public void putKillList(HueOniPlayer player){chaserList.add(player);}
 
-    public int getRunners(){return runnerList.size();}
-    public int getKillers(){return killerList.size();}
+    public int getNumberOfRunners(){return runnerList.size();}
+    public int getNumberOfChasers(){return chaserList.size();}
 }
