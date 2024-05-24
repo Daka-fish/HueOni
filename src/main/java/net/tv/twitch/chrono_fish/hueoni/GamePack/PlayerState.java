94b5f1707471ed57d550593d3ba65249ebae8d1b
Daka-fish <yamatohidaka24@gmail.com>
2024-05-24 14:25:19 +0900

    シンプル化

--- src/main/java/net/tv/twitch/chrono_fish/hueoni/GamePack/PlayerState.java
+++ src/main/java/net/tv/twitch/chrono_fish/hueoni/GamePack/PlayerState.java
@@ -1,18 +1,6 @@
 package net.tv.twitch.chrono_fish.hueoni.GamePack;
 
-import org.bukkit.ChatColor;
-
 public enum PlayerState {
-    Runner(ChatColor.AQUA),
-    Chaser(ChatColor.RED);
-
-    private ChatColor color;
-
-    PlayerState(ChatColor color){
-        this.color = color;
-    }
-
-    public ChatColor getColor() {
-        return color;
-    }
+    Runner,
+    Chaser
 }
