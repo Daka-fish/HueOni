b21a57aa7350c604174aff8cc776874a3780ea01
Daka-fish <yamatohidaka24@gmail.com>
2024-05-25 14:05:49 +0900

    不明なコマンドに対するメッセージを追加

--- src/main/java/net/tv/twitch/chrono_fish/hueoni/CommandPack/HueOniCommands.java
+++ src/main/java/net/tv/twitch/chrono_fish/hueoni/CommandPack/HueOniCommands.java
@@ -1,5 +1,6 @@
 package net.tv.twitch.chrono_fish.hueoni.CommandPack;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -8,7 +9,7 @@ import org.bukkit.entity.Player;
 public class HueOniCommands implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String s, String[] args) {
-        if(sender instanceof Player){
+        if(sender instanceof Player && args.length != 0){
             Player snd = (Player) sender;
             if(command.getName().equalsIgnoreCase("hueOni")){
                 CommandManager.hueOni(snd,args);
