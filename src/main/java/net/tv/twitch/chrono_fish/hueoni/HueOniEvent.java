4ed9cc3a514fe40b3031ee537b8feb37480f293f
Daka-fish <yamatohidaka24@gmail.com>
2024-05-25 13:55:03 +0900

    タッチしたときの処理を追加

--- src/main/java/net/tv/twitch/chrono_fish/hueoni/HueOniEvent.java
+++ src/main/java/net/tv/twitch/chrono_fish/hueoni/HueOniEvent.java
@@ -1,5 +1,8 @@
 package net.tv.twitch.chrono_fish.hueoni;
 
+import net.tv.twitch.chrono_fish.hueoni.GamePack.HueOniGame;
+import net.tv.twitch.chrono_fish.hueoni.GamePack.PlayerState;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -10,7 +13,25 @@ public class HueOniEvent implements Listener {
     @EventHandler
     public void onTouch(EntityDamageByEntityEvent e){
         if(e.getDamager() instanceof Player && e.getEntity() instanceof Player){
+            Player damagerPlayer = (Player) e.getDamager();
+            Player damagedPlayer = (Player) e.getEntity();
 
+            PlayerState damagerState = getPlayerState(damagerPlayer);
+            PlayerState damagedState = getPlayerState(damagedPlayer);
+
+            if(damagerState.equals(PlayerState.Chaser) && damagedState.equals(PlayerState.Runner)){
+                Bukkit.getOnlinePlayers().forEach(player -> player.sendMessage(damagedPlayer.getName()+"が捕まりました"));
+                HueOni.getGame().getMap().put(damagedPlayer,PlayerState.Chaser);
+
+                if(HueOni.getGame().countRunners()==0){
+                    Bukkit.getOnlinePlayers().forEach(player -> player.sendMessage("ゲーム終了!"));
+                    HueOni.getGame().setGameState(HueOniGame.GameState.Finished);
+                }
+            }
         }
+    }
+
+    public PlayerState getPlayerState(Player player){
+        return HueOni.getGame().getMap().get(player);
     }
 }
