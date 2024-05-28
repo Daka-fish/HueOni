e407d18991df46fcfab4fbad47b223a03eb4b7cb
Daka-fish <yamatohidaka24@gmail.com>
2024-05-28 13:49:04 +0900

    フィールドの追加

--- src/main/java/net/tv/twitch/chrono_fish/hueoni/State/GameState.java
+++ src/main/java/net/tv/twitch/chrono_fish/hueoni/State/GameState.java
@@ -1,6 +1,18 @@
 package net.tv.twitch.chrono_fish.hueoni.State;
 
+import org.bukkit.ChatColor;
+
 public enum GameState{
-    Running,
-    Finished
+    Running(ChatColor.GREEN+"進行中"),
+    Finished(ChatColor.GRAY+"準備中");
+
+    String name;
+
+    GameState(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
