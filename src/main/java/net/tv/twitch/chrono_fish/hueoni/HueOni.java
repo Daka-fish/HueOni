e56d49e61107990b4c97cfc27f19f42a4b18ec68
Daka-fish <yamatohidaka24@gmail.com>
2024-06-05 12:01:59 +0900

    getInstanceの追加

--- src/main/java/net/tv/twitch/chrono_fish/hueoni/HueOni.java
+++ src/main/java/net/tv/twitch/chrono_fish/hueoni/HueOni.java
@@ -8,9 +8,11 @@ import org.bukkit.plugin.java.JavaPlugin;
 public final class HueOni extends JavaPlugin {
 
     private static HueOniGame hueOniGame;
+    private static HueOni instance;
 
     @Override
     public void onEnable() {
+        instance = this;
         hueOniGame = new HueOniGame();
         getCommand("hueOni").setExecutor(new HueOniCommands());
         getServer().getPluginManager().registerEvents(new HueOniEvent(),this);
@@ -18,5 +20,8 @@ public final class HueOni extends JavaPlugin {
 
     public static HueOniGame getGame(){
         return hueOniGame;
+    }
+    public static HueOni getInstance(){
+        return instance;
     }
 }
