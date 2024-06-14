f354ea72286cb10eb6df76e73d2022caa8f5016b
Daka-fish <yamatohidaka24@gmail.com>
2024-06-14 16:31:35 +0900

    HueOniItemの追加による修正

--- src/main/java/net/tv/twitch/chrono_fish/hueoni/HueOni.java
+++ src/main/java/net/tv/twitch/chrono_fish/hueoni/HueOni.java
@@ -3,17 +3,21 @@ package net.tv.twitch.chrono_fish.hueoni;
 import net.tv.twitch.chrono_fish.hueoni.CommandPack.HueOniCommands;
 import net.tv.twitch.chrono_fish.hueoni.GamePack.HueOniEvent;
 import net.tv.twitch.chrono_fish.hueoni.GamePack.HueOniGame;
+import net.tv.twitch.chrono_fish.hueoni.ItemPack.ItemManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class HueOni extends JavaPlugin {
 
     private static HueOniGame hueOniGame;
     private static HueOni instance;
+    private static ItemManager itemManager;
 
     @Override
     public void onEnable() {
         instance = this;
         hueOniGame = new HueOniGame();
+        itemManager = new ItemManager();
+        itemManager.loadItems();
         getCommand("hueOni").setExecutor(new HueOniCommands());
         getServer().getPluginManager().registerEvents(new HueOniEvent(),this);
     }
@@ -23,5 +27,8 @@ public final class HueOni extends JavaPlugin {
     }
     public static HueOni getInstance(){
         return instance;
+    }
+    public static ItemManager getItemManager(){
+        return itemManager;
     }
 }
